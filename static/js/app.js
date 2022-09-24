0af24731f73727f49cc0fca846f1dae13bb18bd0
Bren42 <nytech42@gmail.com>
2022-09-24 08:34:42 -0600

    added final updates to css and html to unify styling

--- static/js/app.js
+++ static/js/app.js
@@ -9,16 +9,15 @@ var tbody = d3.select("tbody");
 function buildTable(data) {
     tbody.html("");
 
-// add the foreach function
-    // Next, loop through each object in the data
-    // and append a row and cells for each value in the row
+      // add the foreach function and loop through the data, append a row for each value in row.
+
     data.forEach((dataRow) => {
         // Append a row to the table body
         let row = tbody.append("tr");
         // Loop through each field in the dataRow and add
         // each value as a table cell (td)
         Object.values(dataRow).forEach((val) => {
-                let cell = row.append("td");
+            let cell = row.append("td");
                 cell.text(val);
             }
         );
@@ -26,22 +25,23 @@ function buildTable(data) {
 }
 
 // Create a new function that listens to and handles what occurs after an input is given
-function handleClick(){
+function handleClick() {
     let date = d3.select("#datetime").property("value");
-    let filterData= tableData;
+    let filteredData = tableData;
 
     // create an if statement for JS to check the date, if one is present return only the data with a date.
-    if(date) {
-        filterData = filterData.filter(row =>row.datetime === date);
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
     }
     // Rebuild the table using the filtered data
-  // @NOTE: If no date was entered, then filteredData will
-  // just be the original tableData.
-    buildTable(filterData);
-// Attach an event to listen for the form button
-    d3.select("#filter-btn").on("click", handleClick);
-// Build the table when the page loads
-    buildTable(tableData);
-}
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+    // Attach an event to listen for the form button
 
+    // Build the table when the page loads
+
+}
+d3.selectAll("#filter-btn").on("click", handleClick);
+buildTable(tableData);
 
